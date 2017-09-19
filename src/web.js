30dfd501467c5e74bef70c4d46a9a17f2355233d
Marc-Antoine FERNANDES <embraser01@gmail.com>
2017-09-19 09:49:36 +0200

    Web part

--- src/web.js
+++ src/web.js
@@ -1,84 +1,99 @@
-
 const express = require('express');
+const fs = require('fs');
+const { format } = require('util');
 
-// Application
+const { IF_SECTION, EXPORT_FOLDER, FILE_NAME, PORT } = require('./constants');
 
-const app = express();
+const webApp = express();
+const mainRouter = new express.Router();
 
-//===============//
-//===== WEB =====//
-//===============//
-
-
-app.get('/export/:num_year(\\d+)/:num_group(\\d+)', function (req, res, next) {
-
-    let num_year = Number(req.params.num_year);
-    let num_group = Number(req.params.num_group);
-
-    // Si le groupe et l'année existent
-    if (IF_SECTION[num_year] && IF_SECTION[num_year][num_group - IF_SECTION[num_year][0]]) {
-
-        return res.sendFile(EXPORT_FOLDER + num_year + '/' + util.node.format(FILE_NAME, num_group));
-    }
-
-    next();
-});
-
-app.get('/rss/:num_year(\\d+)/:num_group(\\d+)', function (req, res, next) {
-
-    let num_year = Number(req.params.num_year);
-    let num_group = Number(req.params.num_group);
-
-    // Si le groupe et l'année existent
-    if (IF_SECTION[num_year] && IF_SECTION[num_year][num_group - IF_SECTION[num_year][0]]) {
-
-        return res.send(feeds[num_year][num_group].raw);
-    }
-
-    next();
-});
-
-
-// Create HTTP or HTTPS server.
+let CONFIG = {};
 let server;
 
-if (CONFIG.ssl) {
-    if (!CONFIG.sslKey || !CONFIG.sslCert) {
-        console.error('Cannot start HTTPS server, `sslKey` or `sslCert`' +
-            ' is missing in config.js.');
-        return;
-    }
-
-    server = require('https').createServer({
-        key: fs.readFileSync(CONFIG.sslKey),
-        cert: fs.readFileSync(CONFIG.sslCert)
-    }, app);
-} else {
-    server = require('http').createServer(app);
+/**
+ * Utility to know if a tuple (year, group) exists
+ * @param year {Number} Year (e.g. 4)
+ * @param group {Number} Group (e.g. 2)
+ * @returns {boolean} true if the group exists
+ */
+function groupExists(year, group) {
+    return !IF_SECTION[year] || IF_SECTION[year].indexOf(group) === -1;
 }
 
+/**
+ * Route to the ICS File
+ */
+mainRouter.get('/export/:year(\\d+)/:group(\\d+)', (req, res, next) => {
+    const year = +req.params.year;
+    const group = +req.params.group;
 
-// Listen on provided port, on all network interfaces.
-server.listen(PORT);
-server.on('error', onError);
-server.on('listening', onListening);
+    if (!groupExists(year, group)) return next();
 
+    return res.sendFile(EXPORT_FOLDER + year + '/' + format(FILE_NAME, group));
+});
 
+/**
+ * Route for the RSS Feed
+ */
+mainRouter.get('/rss/:year(\\d+)/:group(\\d+)', (req, res, next) => {
+    const year = +req.params.year;
+    const group = +req.params.group;
+
+    if (!groupExists(year, group)) return next();
+
+    return res.send(feeds[year][group].raw);
+});
+
+webApp.use('', mainRouter);
+
+/**
+ * Start the http server with SSL Configuration if wanted
+ */
+function startExpressApp() {
+    // Create HTTP or HTTPS server.
+    if (CONFIG.ssl) {
+        if (!CONFIG.sslKey || !CONFIG.sslCert) {
+            throw new Error("Cannot start HTTPS server, `sslKey` or `sslCert` is missing in config.js.")
+        }
+
+        server = require('https').createServer({
+            key: fs.readFileSync(CONFIG.sslKey),
+            cert: fs.readFileSync(CONFIG.sslCert)
+        }, webApp);
+    } else {
+        server = require('http').createServer(webApp);
+    }
+
+    // Listen on provided port, on all network interfaces.
+    server.listen(PORT);
+    server.on('error', onError);
+    server.on('listening', () => {
+        const addr = server.address();
+        const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+        console.log('Listening on ' + bind);
+    });
+}
+
+/**
+ * Function called when an error is thrown by the http server
+ * It can stop the entire process
+ * @param error
+ */
 function onError(error) {
     if (error.syscall !== 'listen') {
         throw error;
     }
 
-    let bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
     // Handle specific listen errors with friendly messages.
     switch (error.code) {
         case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
+            console.error(`Web app :${bind} requires elevated privileges`);
             process.exit(1);
             break;
         case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
+            console.error(`Web app :${bind} is already in use`);
             process.exit(1);
             break;
         default:
@@ -86,9 +101,17 @@ function onError(error) {
     }
 }
 
-// Event listener for HTTP server "listening" event.
-function onListening() {
-    let addr = server.address();
-    let bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
-    console.log('Listening on ' + bind);
-}
+
+module.exports = {
+    start(config) {
+        CONFIG.ssl = !!config.ssl;
+        CONFIG.sslCert = config.sslCert;
+        CONFIG.sslKey = config.sslKey;
+
+        startExpressApp();
+    },
+
+    stop() {
+        server.close();
+    }
+};
--- test/parser.js
+++ test/parser.js
@@ -1,9 +1,50 @@
 const should = require('should');
+const cheerio = require('cheerio');
+const moment = require('moment');
+
+const parser = require('../src/parser');
+const {PlanningEvent} = require('../src/models');
+const {EVENT_SELECTOR} = require('../src/constants');
 
 describe('Parsing', () => {
-    describe('parse', () => {
-        it('should return ', () => {
+    describe('event', () => {
+        it('should return the correct event', () => {
+            const $ = cheerio.load(`
+                <th class="hour-header row-group-1">4IF1</th>
+                    <td id="slot-S43-J1-0-0" colspan="19" rowspan="1" class="Slot-TP Title-504C44204D415253">
+                        <table class="container">
+                            <tr>
+                                <td colspan="2" style="text-align: center;">PLD MARS [TP-Gr.4IF1]</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align: left;">08h00 @&nbsp;501:212, 501:213 </td>
+                                <td style="text-align: right;">Y.&nbsp;Gripay, S.&nbsp;Servigne </td>
+                            </tr>
+                        </table>
+                    </td>
+            `);
+
+            const event = parser.parseEvent($, $(EVENT_SELECTOR));
+
+            event.should.be.instanceOf(PlanningEvent);
+            event.should.have.property('groups').with.lengthOf(1).containEql(1);
+            event.should.have.property('title').which.equal('PLD MARS [TP-Gr.4IF1]');
+            event.should.have.property('description').which.startWith('Y. Gripay, S. Servigne');
+            event.should.have.property('location').which.startWith('501:212, 501:213');
+            event.should.have.property('start').which.is.Date().and.eql(moment('2017-10-23 08:00:00').toDate());
+            event.should.have.property('end').which.is.Date().and.eql(moment('2017-10-23 12:00:00').toDate());
+        });
+
+        it('should return an event without location if "-"', () => {
 
         });
+
+        it('should return an event without if not present', () => {
+
+        });
+    });
+
+    describe('document HTML', () => {
+
     });
 });
